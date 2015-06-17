5e445dcc4579923fe5fbf551e3a9001334667ac3
dengyh <dengyh071@gmail.com>
2015-06-17 14:32:04 +0800

    Complete comment function

--- code/comment/urls.py
+++ code/comment/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.get_comments, {'templateName': 'comments.html'}, name='comments'),
+    url(r'^create/$', views.create_comment, name='create'),
 ]
